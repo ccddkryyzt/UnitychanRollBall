5a527dcf9749acf1ce51dd72ed109a1d4350254f
ccddkryyzt <0zr3mv0272@gmail.com>
2016-03-21 16:02:43 +0900

    ファーストコミット

---
+++ Assets/_MyGame_takahashi/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float moveHorizontal;
+    private float moveVertical;
+    private Rigidbody rb;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //inputはUpdate
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        //動かすのはfixedUpdate
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // アイテムゲット
+        if (other.gameObject.CompareTag("Item"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/_MyGame_takahashi/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    void Update()
+    {
+        this.transform.Rotate(new Vector3(10, 30, 45) * Time.deltaTime);
+    }
+}
